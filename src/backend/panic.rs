28f7d55f308e36dcd97c182c4221bf5998cf5339
Alisa Sireneva <me@purplesyringa.moe>
2024-10-26 12:45:22 +0300

    Translate panic backend to new design

--- src/backend/panic.rs
+++ src/backend/panic.rs
@@ -1,72 +1,32 @@
-use crate::{
-    exceptions::{recover_last, Exception},
-    InFlightException,
-};
-use std::any::Any;
+use super::Backend;
 use std::panic::{catch_unwind, resume_unwind, AssertUnwindSafe};
-
-pub struct StackPanicException;
 
 pub struct ActiveBackend;
 
-impl Backend for ActiveBackend {dassadsa}
+unsafe impl Backend for ActiveBackend {
+    type ExceptionHeader = LithiumMarker;
 
-#[repr(C)]
-pub struct Header;
-
-impl Header {
-    pub fn new() -> Self {
-        Self
+    fn new_header() -> LithiumMarker {
+        LithiumMarker
     }
-}
 
-unsafe impl<E> Send for Exception<E> {}
-
-pub unsafe fn throw<E>(is_local: bool, ex: *mut Exception<E>) -> ! {
-    let ex = if is_local {
-        // StackPanicException is a ZST, so this avoids an allocation
-        Box::new(StackPanicException)
-    } else {
+    unsafe fn throw(ex: *mut LithiumMarker) -> ! {
         let ex = unsafe { Box::from_raw(ex) };
-        unsafe { to_any(ex) }
-    };
-    std::panic::resume_unwind(ex);
-}
+        std::panic::resume_unwind(ex);
+    }
 
-pub unsafe fn intercept<Func: FnOnce() -> R, R, E>(func: Func) -> Result<R, InFlightException<E>> {
-    match catch_unwind(AssertUnwindSafe(func)) {
-        Ok(value) => Ok(value),
-        Err(ex) => {
-            let ex = if ex.is::<StackPanicException>() {
-                unsafe { recover_last::<E>() }
-            } else if (*ex).type_id() == typeid::of::<Exception<E>>() {
-                Box::into_raw(ex).cast()
-            } else {
-                resume_unwind(ex);
-            };
-            Err(unsafe { InFlightException::new(ex) })
+    unsafe fn intercept<Func: FnOnce() -> R, R>(func: Func) -> Result<R, *mut LithiumMarker> {
+        match catch_unwind(AssertUnwindSafe(func)) {
+            Ok(value) => Ok(value),
+            Err(ex) => {
+                if ex.is::<LithiumMarker>() {
+                    Err(Box::into_raw(ex).cast())
+                } else {
+                    resume_unwind(ex);
+                }
+            }
         }
     }
 }
 
-unsafe fn to_any<T: Send>(value: Box<T>) -> Box<dyn Any + Send> {
-    unsafe {
-        std::mem::transmute::<Box<dyn NonStaticAny + '_>, Box<dyn NonStaticAny + 'static>>(value)
-    }
-    .to_any()
-}
-
-trait NonStaticAny: Send {
-    fn to_any(self: Box<Self>) -> Box<dyn Any + Send>
-    where
-        Self: 'static;
-}
-
-impl<T: Send> NonStaticAny for T {
-    fn to_any(self: Box<Self>) -> Box<dyn Any + Send>
-    where
-        Self: 'static,
-    {
-        self
-    }
-}
+pub struct LithiumMarker;
