86e30f8fafc8e25ed864b7504a9fad956871ac5a
Alisa Sireneva <me@purplesyringa.moe>
2024-10-27 18:17:33 +0300

    Enable backend configuration via env for testing

--- build.rs
+++ build.rs
@@ -7,13 +7,19 @@ fn main() {
     if is_miri && !is_tree_borrows {
         println!("cargo::rustc-cfg=feature=\"sound-under-stacked-borrows\"");
     }
-    if version_meta().unwrap().channel == Channel::Nightly
-        && (std::env::var_os("CARGO_CFG_UNIX").is_some()
-            || (std::env::var_os("CARGO_CFG_WINDOWS").is_some()
-                && std::env::var_os("CARGO_CFG_TARGET_ENV").is_some_and(|env| env == "gnu")))
-    {
-        println!("cargo::rustc-cfg=backend=\"itanium\"");
+
+    println!("cargo::rerun-if-env-changed=LITHIUM_BACKEND");
+    if let Ok(backend) = std::env::var("LITHIUM_BACKEND") {
+        println!("cargo::rustc-cfg=backend=\"{backend}\"");
     } else {
-        println!("cargo::rustc-cfg=backend=\"panic\"");
+        if version_meta().unwrap().channel == Channel::Nightly
+            && (std::env::var_os("CARGO_CFG_UNIX").is_some()
+                || (std::env::var_os("CARGO_CFG_WINDOWS").is_some()
+                    && std::env::var_os("CARGO_CFG_TARGET_ENV").is_some_and(|env| env == "gnu")))
+        {
+            println!("cargo::rustc-cfg=backend=\"itanium\"");
+        } else {
+            println!("cargo::rustc-cfg=backend=\"panic\"");
+        }
     }
 }
