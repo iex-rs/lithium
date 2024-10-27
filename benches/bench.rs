aba9b78fbcf743ce9e4981756d95f0710390395f
Alisa Sireneva <me@purplesyringa.moe>
2024-10-27 21:38:52 +0300

    More info on benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,49 +1,92 @@
-use anyhow::{anyhow, Error};
+use anyhow::anyhow;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use lithium::{catch, intercept, throw};
 
-fn rust() {
-    fn imp(n: u32) {
-        let n = black_box(n);
-        if n == 0 {
-            std::panic::resume_unwind(Box::new(anyhow!("Hello, world!")));
-        } else {
-            let mut bx = std::panic::catch_unwind(|| imp(n - 1)).unwrap_err();
-            let err = bx.downcast_mut::<Error>().unwrap();
-            replace_with::replace_with_or_abort(err, |e| e.context("In imp"));
-            std::panic::resume_unwind(bx);
-        }
-    }
-    let _ = black_box(std::panic::catch_unwind(|| {
-        imp(5);
-    }));
-}
-
-fn lithium() {
-    fn imp(n: u32) {
-        let n = black_box(n);
-        unsafe {
+fn bench_anyhow(c: &mut Criterion) {
+    fn rust() {
+        fn imp(n: u32) {
+            let n = black_box(n);
             if n == 0 {
-                throw(anyhow!("Hello, world!"));
+                std::panic::resume_unwind(Box::new(anyhow!("Hello, world!")));
             } else {
-                let (e, in_flight) = intercept::<(), Error>(|| imp(n - 1)).unwrap_err();
-                in_flight.rethrow(e.context("In imp"));
+                let mut bx = std::panic::catch_unwind(|| imp(n - 1)).unwrap_err();
+                let err = bx.downcast_mut::<anyhow::Error>().unwrap();
+                replace_with::replace_with_or_abort(err, |e| e.context("In imp"));
+                std::panic::resume_unwind(bx);
             }
         }
-    }
-    let _ = black_box(unsafe {
-        catch::<(), Error>(|| {
+        let _ = black_box(std::panic::catch_unwind(|| {
             imp(5);
-        })
-    });
-}
+        }));
+    }
 
-fn bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Exceptions");
+    fn lithium() {
+        fn imp(n: u32) {
+            let n = black_box(n);
+            unsafe {
+                if n == 0 {
+                    throw(anyhow!("Hello, world!"));
+                } else {
+                    let (e, in_flight) = intercept::<(), anyhow::Error>(|| imp(n - 1)).unwrap_err();
+                    in_flight.rethrow(e.context("In imp"));
+                }
+            }
+        }
+        let _ = black_box(unsafe {
+            catch::<(), anyhow::Error>(|| {
+                imp(5);
+            })
+        });
+    }
+
+    let mut group = c.benchmark_group("anyhow");
     group.bench_function("Rust", |b| b.iter(|| rust()));
     group.bench_function("Lithium", |b| b.iter(|| lithium()));
     group.finish();
 }
 
-criterion_group!(benches, bench);
+fn bench_simple(c: &mut Criterion) {
+    fn rust() {
+        fn imp(n: u32) {
+            let n = black_box(n);
+            if n == 0 {
+                std::panic::resume_unwind(Box::new("Hello, world!"));
+            } else {
+                let mut bx = std::panic::catch_unwind(|| imp(n - 1)).unwrap_err();
+                let err = bx.downcast_mut::<&'static str>().unwrap();
+                black_box(err); // simulate adding information to the error in some fashion
+                std::panic::resume_unwind(bx);
+            }
+        }
+        let _ = black_box(std::panic::catch_unwind(|| {
+            imp(5);
+        }));
+    }
+
+    fn lithium() {
+        fn imp(n: u32) {
+            let n = black_box(n);
+            unsafe {
+                if n == 0 {
+                    throw("Hello, world!");
+                } else {
+                    let (e, in_flight) = intercept::<(), &'static str>(|| imp(n - 1)).unwrap_err();
+                    in_flight.rethrow(black_box(e)); // simulate adding information
+                }
+            }
+        }
+        let _ = black_box(unsafe {
+            catch::<(), &'static str>(|| {
+                imp(5);
+            })
+        });
+    }
+
+    let mut group = c.benchmark_group("simple");
+    group.bench_function("Rust", |b| b.iter(|| rust()));
+    group.bench_function("Lithium", |b| b.iter(|| lithium()));
+    group.finish();
+}
+
+criterion_group!(benches, bench_anyhow, bench_simple);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,11 @@
 //! panics, this mechanism is allocation-free, avoids indirections and RTTI, and hence faster, if
 //! less applicable.
 //!
-//! When used together with the [`revolve`](https://lib.rs/revolve) crate, unwinding exceptions
-//! takes only 3 ns/frame, barely slower than returns. In this configuration, using exceptions
-//! instead of [`Result`] both speeds up the success path and only marginally slows down the error
-//! path, making this approach ideal when the probability of an error is low.
+//! Under the default configuration, Lithium is more than 2x faster Rust panics on common
+//! `Result`-like usecases. When used together with the [`revolve`](https://lib.rs/revolve) crate,
+//! it's 2-3x faster than Rust panics, bringing timings down to about 15 ns per frame with `map_err`
+//! in realistic code -- faster than an allocation. See the
+//! [benchmark](https://github.com/iex-rs/lithium/blob/master/benches/bench.rs).
 //!
 //!
 //! # Usage
