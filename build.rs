15e4ce02d250e73896a8321f28ce01e5c9075b9d
Alisa Sireneva <me@purplesyringa.moe>
2025-01-17 04:33:02 +0300

    Fix nightly-only code on stable

--- build.rs
+++ build.rs
@@ -17,21 +17,20 @@ fn main() {
         println!("cargo::rustc-cfg=feature=\"sound-under-stacked-borrows\"");
     }
 
+    let is_nightly = version_meta().unwrap().channel == Channel::Nightly;
+
     println!("cargo::rerun-if-env-changed=LITHIUM_BACKEND");
     if let Ok(backend) = std::env::var("LITHIUM_BACKEND") {
         println!("cargo::rustc-cfg=backend=\"{backend}\"");
-    } else if cfg("target_os") == "emscripten" {
+    } else if is_nightly && cfg("target_os") == "emscripten" {
         println!("cargo::rustc-cfg=backend=\"emscripten\"");
-    } else if version_meta().unwrap().channel == Channel::Nightly
+    } else if is_nightly
         && (has_cfg("unix")
             || (has_cfg("windows") && cfg("target_env") == "gnu")
             || cfg("target_arch") == "wasm32")
     {
         println!("cargo::rustc-cfg=backend=\"itanium\"");
-    } else if version_meta().unwrap().channel == Channel::Nightly
-        && (has_cfg("windows") && cfg("target_env") == "msvc")
-        && !is_miri
-    {
+    } else if is_nightly && (has_cfg("windows") && cfg("target_env") == "msvc") && !is_miri {
         println!("cargo::rustc-cfg=backend=\"seh\"");
     } else {
         #[cfg(feature = "std")]
