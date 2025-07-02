647c33ccb742058500c7f009659f4e01ce90e0a2
Alisa Sireneva <me@purplesyringa.moe>
2025-07-02 11:44:25 +0300

    Update WASI EH implementation in docs

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,7 @@
 //! |Windows (GNU ABI)  |Itanium EH ABI |2.5x faster than panics, but slower than MSVC|
 //! |Emscripten (old EH)|C++ exceptions |2x faster than panics                        |
 //! |Emscripten (new EH)|Wasm exceptions|2.5x faster than panics                      |
-//! |WASI               |Itanium EH ABI |2.5x faster than panics                      |
+//! |WASI               |Wasm exceptions|2.5x faster than panics                      |
 //!
 //! Lithium strives to support all targets that Rust panics support. If Lithium does not work
 //! correctly on such a target, please [open an issue](https://github.com/iex-rs/lithium/issues/).
